f5594bfe8415259bbd9c73683a4783e4befb87fd
Safi <safikurrahman7514@gmail.com>
2022-02-17 20:16:46 +0600

    error message done

--- js/main.js
+++ js/main.js
@@ -19,38 +19,50 @@ function montlyExpenses() {
     const currency = document.getElementById('balance');
     const bdCurrency = parseFloat(incomeValue - montlyExpensesCount);
     currency.innerText = bdCurrency;
-    // Error Handle
-    if (foodCostValue < 0 && foodCostValue < incomeValue) {
-
+    // Error message food rent and clothes
+    if (isNaN(foodCostValue) || foodCostValue < 0) {
+        const foodCost = document.getElementById('food');
+        foodCost.value = 'Please Valid Food Number'
+    } else if (isNaN(houseRentValue) || houseRentValue < 0) {
+        const houseRent = document.getElementById('rent');
+        houseRent.value = 'Please Valid House Rent Number'
+    } else if (isNaN(clothesCostValue) || clothesCostValue < 0) {
+        const clothesCost = document.getElementById('clothes');
+        clothesCost.value = 'Please Valid Clothes Number'
     }
-}
 
+    // Error Handle override montlyexpenses
+    if (montlyExpensesCount > incomeValue) {
+        alert('You have expenses over in this month')
+    };
+};
 // Event handle calculate button
 
 document.getElementById('calculate-btn').addEventListener('click', function () {
-    montlyExpenses()
+    montlyExpenses();
 });
 
 // Saving Amount event handler button
 document.getElementById('saving-amount-btn').addEventListener('click', function () {
-    // montlyExpenses('balance')
-    savingAmount()
+    savingAmount();
 });
 // saving amount 
 function savingAmount() {
+    const totalIncome = document.getElementById('income');
+    const incomeValue = totalIncome.value;
     const balance = document.getElementById('balance');
     const balanceText = parseFloat(balance.innerText);
 
     const percentageField = document.getElementById('percentage-field');
     const percentageValue = parseFloat(percentageField.value);
 
-    const percentage = balanceText / 100 * percentageValue;
+    const percentage = incomeValue / 100 * percentageValue;
     document.getElementById('saving-amount').innerText = percentage;
     document.getElementById('remaining-balance').innerText = balanceText - percentage;
 
     //Error Handle
     if (percentage > balanceText) {
         alert("You haven't enough money for doing save")
-    }
+    };
 
 };
